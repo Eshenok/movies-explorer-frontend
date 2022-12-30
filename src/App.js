2b496d6d73348ba5273cb2d4d188c2df6ccf1d3c
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-12-30 22:47:55 +0600

    make navTab Hero for all display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import personalPhoto from './images/personalphoto.jpg';
 import './App.css';
 import Header from './components/Header/Header'
-
+import Hero from "./components/Hero/Hero";
+import NavTab from "./components/NavTab/NavTab";
 function App() {
 
 
@@ -10,35 +11,25 @@ function App() {
     <>
       <Header />
       <main className="main">
-        <section className="hero">
-          <h1 className="hero__title">Учебный проект студента факультета Веб-разработки.</h1>
-        </section>
-
-        <section className="navTab">
-          <ul className="navTab__links">
-            <a className="navTab__link" href="#"><li>О проекте</li></a>
-            <a className="navTab__link" href="#"><li>Технологии</li></a>
-            <a className="navTab__link" href="#"><li>Студент</li></a>
-          </ul>
-        </section>
-
-        <section className="promo">
-          <h2 className="promo__title">О проекте</h2>
+        <Hero />
+        <NavTab />
+        <section className="section promo">
+          <h2 className="section__title section__title_border_white promo__title">О проекте</h2>
           <div className="promo__container">
             <article className="promo__brief">
-              <h3 className="promo__subtitle">Дипломный проект включал 5 этапов</h3>
-              <p className="promo__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+              <h3 className="section__title promo__subtitle">Дипломный проект включал 5 этапов</h3>
+              <p className="section__text promo__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
             </article>
             <article className="promo__brief">
-              <h3 className="promo__subtitle">На выполнение диплома ушло 5 недель</h3>
-              <p className="promo__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+              <h3 className="section__title promo__subtitle">На выполнение диплома ушло 5 недель</h3>
+              <p className="section__text promo__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
             </article>
           </div>
           <div className="promo__time">
             <p className="promo__block promo__block_theme_green">1 неделя</p>
             <p className="promo__block promo__block_theme_grey">4 недели</p>
-            <p className="promo__block-caption">Back-end</p>
-            <p className="promo__block-caption">Front-end</p>
+            <p className="promo__block_caption">Back-end</p>
+            <p className="promo__block_caption">Front-end</p>
           </div>
         </section>
 
