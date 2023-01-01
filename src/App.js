ad2351380b12edb62fd155de2613d947a8461969
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-01-02 02:27:09 +0600

    make tech for all display

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header/Header'
 import Hero from "./components/Hero/Hero";
 import NavTab from "./components/NavTab/NavTab";
+import Promo from "./components/Promo/Promo";
+import Tech from "./components/Tech/Tech";
 function App() {
 
 
@@ -13,51 +15,21 @@ function App() {
       <main className="main">
         <Hero />
         <NavTab />
-        <section className="section promo">
-          <h2 className="section__title section__title_border_white promo__title">О проекте</h2>
-          <div className="promo__container">
-            <article className="promo__brief">
-              <h3 className="section__title promo__subtitle">Дипломный проект включал 5 этапов</h3>
-              <p className="section__text promo__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-            </article>
-            <article className="promo__brief">
-              <h3 className="section__title promo__subtitle">На выполнение диплома ушло 5 недель</h3>
-              <p className="section__text promo__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-            </article>
-          </div>
-          <div className="promo__time">
-            <p className="promo__block promo__block_theme_green">1 неделя</p>
-            <p className="promo__block promo__block_theme_grey">4 недели</p>
-            <p className="promo__block_caption">Back-end</p>
-            <p className="promo__block_caption">Front-end</p>
-          </div>
-        </section>
-
-        <section className="tech">
-          <h2 className="tech__title">Технологии</h2>
-          <h3 className="tech__subtitle">7 технологий</h3>
-          <p className="tech__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-          <div className="tech__tile">
-            <p className="tech__tile_item">HTML</p>
-            <p className="tech__tile_item">CSS</p>
-            <p className="tech__tile_item">JS</p>
-            <p className="tech__tile_item">React</p>
-            <p className="tech__tile_item">Git</p>
-            <p className="tech__tile_item">Express.js</p>
-            <p className="tech__tile_item">MongoDB</p>
-          </div>
-        </section>
-
+        <Promo />
+        <Tech />
         <section className="student">
-          <h2 className="student__title">Студент</h2>
-          <img className="student__image" alt="Портрет. Александр Волошин"/>
-          <h3 className="student__name">Александр</h3>
-          <p className="student__text">Фронтенд-разработчик, 23 года</p>
-          <p className="student__text">Я родился и вырос в Москве, закончил Политехнический колледж по специальности "Прикладная информатика".
-            Мое хобби: Гитара, 10+ лет, играю в стиле fingerstyle. Ранее работал в Интернет-провайдере и сети кинотеатров.
-            В 2021 году решил сменить профессию и начал учиться на курсах Яндекс.Практикум.</p>
+          <h2 className="section__title section__title_border_white student__title">Студент</h2>
+          <img className="student__image" src={personalPhoto} alt="Портрет. Александр Волошин"/>
+          <div className="student__container">
+            <h3 className="student__name">Александр</h3>
+            <p className="section__text student__text">Фронтенд-разработчик, 23 года</p>
+            <p className="section__text student__text">Я родился и вырос в Москве, закончил Политехнический колледж по специальности "Прикладная информатика".
+              Мое хобби: Гитара, 10+ лет, играю в стиле fingerstyle. Ранее работал в Интернет-провайдере и сети кинотеатров.
+              В 2021 году решил сменить профессию и начал учиться на курсах Яндекс.Практикум.</p>
+          </div>
           <a className="student__ghlink">Github</a>
         </section>
+
         <section className="portfolio">
           <h3 className="portfolio__title">Портфолио</h3>
           <ul className="portfolio__links">
