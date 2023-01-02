7569dbb0b153cd12dd570f089829f7d64290a33e
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-01-02 21:52:08 +0600

    make Student for all display

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import NavTab from "./components/NavTab/NavTab";
 import Promo from "./components/Promo/Promo";
 import Tech from "./components/Tech/Tech";
+import Student from "./components/Student/Student";
 function App() {
 
 
@@ -17,18 +18,7 @@ function App() {
         <NavTab />
         <Promo />
         <Tech />
-        <section className="student">
-          <h2 className="section__title section__title_border_white student__title">Студент</h2>
-          <img className="student__image" src={personalPhoto} alt="Портрет. Александр Волошин"/>
-          <div className="student__container">
-            <h3 className="student__name">Александр</h3>
-            <p className="section__text student__text">Фронтенд-разработчик, 23 года</p>
-            <p className="section__text student__text">Я родился и вырос в Москве, закончил Политехнический колледж по специальности "Прикладная информатика".
-              Мое хобби: Гитара, 10+ лет, играю в стиле fingerstyle. Ранее работал в Интернет-провайдере и сети кинотеатров.
-              В 2021 году решил сменить профессию и начал учиться на курсах Яндекс.Практикум.</p>
-          </div>
-          <a className="student__ghlink">Github</a>
-        </section>
+        <Student />
 
         <section className="portfolio">
           <h3 className="portfolio__title">Портфолио</h3>
