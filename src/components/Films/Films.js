1cdafde1d373cce248e2abb2a22b2ff20dd9916b
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-01-13 23:55:20 +0600

    make films all disp

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -3,12 +3,14 @@ import Film from "../Film/Film";
 export default function Films() {
   return (
     <section className="films">
-      <Film />
-      <Film />
-      <Film />
-      <Film />
-      <Film />
-      <button className="button films__more-button">Еще</button>
+      <div className="films__container">
+        <Film />
+        <Film />
+        <Film />
+        <Film />
+        <Film />
+      </div>
+      <button className="button films__more-button">Ещё</button>
     </section>
   )
 }
